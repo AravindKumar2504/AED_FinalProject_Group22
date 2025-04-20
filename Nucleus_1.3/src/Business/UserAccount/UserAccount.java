0edbd14e9a6121e8c5c1031ef7e4ebba62221960
AraGooner <sundaravadivelu.a@northeastern.edu>
2025-04-19 23:30:46 -0400

    business useraccount and workqueue created

---
+++ Nucleus_1.3/src/Business/UserAccount/UserAccount.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.UserAccount;
+
+import Business.Employee.Employee;
+import Business.WorkQueue.WorkQueue;
+
+/**
+ *
+ * @author Admin
+ */
+public class UserAccount {
+    
+    private String username;
+    private String password;
+    private Employee employee;
+    private WorkQueue workQueue;
+    private String roleName;
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+    
+    
+
+    public UserAccount() {
+        workQueue = new WorkQueue();
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }    
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public WorkQueue getWorkQueue() {
+        return workQueue;
+    }
+
+    
+    
+    @Override
+    public String toString() {
+        return username;
+    }
+    
+    
+    
+}
