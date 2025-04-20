bfa1030d47729dbad54a23d07920a6cd951cd0b3
SathwikMaddula19 <maddula.s@northeastern.edu>
2025-04-20 03:31:16 -0400

    Customer Area UI changed

---
+++ Nucleus_1.8/src/Business/Grocery/Grocery.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Grocery;
+
+
+import Business.UserAccount.UserAccount;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Admin
+ */
+public class Grocery {
+    private String name;
+    private int id;
+    private static int count = 1;
+    private String managerName;    
+    private String location;
+    private String phone;
+    private UserAccount userAccount;
+    private HashMap<String, String> menu;
+    private String Network;
+
+    public String getNetwork() {
+        return Network;
+    }
+
+    public void setNetwork(String Network) {
+        this.Network = Network;
+    }
+
+
+    
+    
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+    public HashMap<String, String> getMenu() {
+        return menu;
+    }
+
+    public void setMenu(HashMap<String, String> menu) {
+        this.menu = menu;
+    }
+    
+    
+    public UserAccount getUserAccount() {
+        return userAccount;
+    }
+
+    public void setUserAccount(UserAccount userAccount) {
+        this.userAccount = userAccount;
+    }
+
+
+    
+    public void setMenuItem(String name, String value) {
+        menu.put(name, value);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public static void setCount(int count) {
+        Grocery.count = count;
+    }
+    
+    
+}
